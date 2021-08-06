7fad0f81ff20abf1bd5352b1cbbeb12000eea47e
EC2 Default User <ec2-user@ip-172-31-44-53.ap-northeast-1.compute.internal>
2021-08-06 09:01:21 +0000

    D-8-8.6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,13 @@ class UsersController < ApplicationController
       render :edit      
     end
    end
-  
+   
+   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+   end
   
   
   private
